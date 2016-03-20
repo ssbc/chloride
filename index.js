19457e05476eb3019c5de3f9d41ca395d718a634
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-21 12:01:26 +1300

    don't try to fool browserify twice, or you'll fool noderify too (there is already a fool browserify thing in ./bindings.js)

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ if(process.env.CHLORIDE_JS)
   return module.exports = require('./browser')
 
 try {
-  var r = require //fool browserify
-  module.exports = r('./bindings')
+  module.exports = require('./bindings')
 } catch (err) {
   console.error('error loading sodium bindings:', err.message)
   console.error('falling back to javascript version.')
   module.exports = require('./browser')
 }
+
