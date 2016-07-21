50dbe3bfdf3973cc4a6e7d8f2d00c24329710b06
Dominic Tarr <dominic.tarr@gmail.com>
2016-07-21 19:46:06 +1200

    detect if we are running in electron, and then check if we need to install workaround

--- index.js
+++ index.js
@@ -2,11 +2,49 @@
 if(process.env.CHLORIDE_JS)
   return module.exports = require('./browser')
 
+function isElectron () {
+  try {
+    require('electron')
+    return true
+  } catch (_) { return false }
+}
+
 try {
-  module.exports = require('./bindings')
+  var cl = module.exports = require('./bindings')
+
+  if(isElectron()) {
+    //there is a weird problem with electro.
+    //where detached signatures do not work, but other
+    //signatures do...
+
+    var keys = cl.crypto_sign_keypair()
+    var msg = cl.crypto_hash(new Buffer('test signature'))
+    var sig = cl.crypto_sign_detached(msg, keys.secretKey)
+
+    if(cl.crypto_sign_verify_detached(sig, msg, keys.publicKey))
+      return
+
+    console.error('detached signatures broken in electron, using workaround')
+
+    var verify = module.exports.crypto_sign_verify_detached
+    module.exports.crypto_sign_verify_detached = function (sig, msg, pk) {
+      //return verify(copy(sig), copy(msg), copy(pk))
+      return module.exports.crypto_sign_open(Buffer.concat([sig, msg]), pk)
+      //console.log(sig, msg, pk)
+//      return verify(new Buffer(sig), new Buffer(msg), new Buffer(pk))
+    }
+  }
 } catch (err) {
   console.error('error loading sodium bindings:', err.message)
   console.error('falling back to javascript version.')
   module.exports = require('./browser')
 }
+
+
+
+
+
+
+
+
 
