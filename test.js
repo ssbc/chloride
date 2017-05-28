4fab384aa2822ee47fcea5f6f2df4c06b9ccfccc
Dominic Tarr <dominic.tarr@gmail.com>
2017-05-28 18:39:01 +1200

    test bindings directly too

--- test.js
+++ test.js
@@ -4,8 +4,10 @@ var test = require('chloride-test')
 //if(process.title === 'node')
   process.exit(
    test(require('./')).fail
+  + test(require('./bindings')).fail
   + test(require('./browser-small')).fail
   + test(require('./browser')).fail
   )
+
 
 
