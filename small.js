068676ff69edfadf1e164f73f8ff6b69ed7c6abd
Christian Bundy <christianbundy@fraction.io>
2019-07-27 08:15:54 -0700

    Fix 'return' outside of function

--- small.js
+++ small.js
@@ -1,12 +1,11 @@
-
-if(process.env.CHLORIDE_JS)
-  return module.exports = require('./browser-small')
-
-try {
-  module.exports = require('./bindings')
-} catch (err) {
-  console.error('error loading sodium bindings:', err.message)
-  console.error('falling back to javascript version.')
+if (process.env.CHLORIDE_JS) {
   module.exports = require('./browser-small')
+} else {
+  try {
+    module.exports = require('./bindings')
+  } catch (err) {
+    console.error('error loading sodium bindings:', err.message)
+    console.error('falling back to javascript version.')
+    module.exports = require('./browser-small')
+  }
 }
-
