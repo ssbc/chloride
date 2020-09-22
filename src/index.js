52506e8238de9e5c2f44e4ab26f459b086de29a8
Christian Bundy <christianbundy@fraction.io>
2020-09-22 08:44:08 -0700

    Switch to Sodium-Native and Sodium-JavaScript
    Previously we've been using Sodium-Native and libsodium-wrappers, with
    an option to use TweetNaCl, but libsodium-wrappers is unnecessarily slow
    and large and TweetNaCl is rarely used.
    
    This commit switches the dependencies to Sodium-Native and
    Sodium-Javascript exclusively, which are smaller, faster, and share an
    API.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
-var isElectron = require('is-electron')
+const sodiumChloride = require('./sodium-chloride')
 
-const cl = require('./sodium-chloride')(require('sodium-universal'))
-module.exports = cl;
-
-if(isElectron()) {
-  //there is a weird problem with electro.
-  //where detached signatures do not work, but other
-  //signatures do...
-
-  var keys = cl.crypto_sign_keypair()
-  var msg = cl.crypto_hash(Buffer.from('test signature'))
-  var sig = cl.crypto_sign_detached(msg, keys.secretKey)
-
-  if(!cl.crypto_sign_verify_detached(sig, msg, keys.publicKey)) {
-    console.error('detached signatures broken in electron, using workaround')
-
-    var verify = module.exports.crypto_sign_verify_detached
-    module.exports.crypto_sign_verify_detached = function (sig, msg, pk) {
-      return module.exports.crypto_sign_open(Buffer.concat([sig, msg]), pk)
-    }
-  }
+try {
+  module.exports = sodiumChloride(require('sodium-native'))
+} catch (e) {
+  module.exports = sodiumChloride(require('sodium-javascript'))
 }
